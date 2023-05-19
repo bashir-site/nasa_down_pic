328557d9f17fd33b387b0c3022e22a63f5f61d16
bashir-site <59281683+bashir-site@users.noreply.github.com>
2023-05-19 10:23:52 +0300

    Ввел свой тип исключений

--- public_photo.py
+++ public_photo.py
@@ -5,13 +5,19 @@ import telegram
 from dotenv import load_dotenv
 
 
+class EmptyListError(Exception):
+    def __init__(self, text):
+        self.txt = text
+
+
 def send_photo(folder, shuffle):
     pictures_dir = os.listdir('{}/'.format(folder))
     if shuffle:
         file = random.shuffle(pictures_dir)
     for picture_number, picture in enumerate(pictures_dir):
+        if pictures_dir == []:
+            raise EmptyListError("Список фотогафий пуст!")
         file = pictures_dir.pop(picture_number)
-        print(file)
         with open('{}/{}'.format(folder, file), 'rb') as photo:
             bot.send_document(chat_id=tg_chat_id, document=photo)
 
@@ -25,8 +31,8 @@ if __name__ == "__main__":
     epic_folder, nasa_folder, images_folder = 'epic', 'nasa', 'images'
 
     try:
-        schedule.every(2).seconds.do(send_photo, folder=epic_folder, shuffle=False)
-    except IndexError:
+        schedule.every(14400).seconds.do(send_photo, folder=epic_folder, shuffle=False)
+    except EmptyListError as elr:
         schedule.every(14400).seconds.do(send_photo, folder=epic_folder, shuffle=True)
 
     while True:
